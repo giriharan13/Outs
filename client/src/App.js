7e4df33ecf58dbc35af2189fc138b2a63b4b18c8
InquisitiveKrishna <krishnacharankvs@gmail.com>
2024-08-05 07:03:33 +0530

    fix : Added SignIn and SighUp codes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import './App.css';
-
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 function App() {
+  const [currentForm,setCurrentFrom]=useState('login')
+  const [email,setEmail]=useState("");
+  const [pass,setPass]=useState("");
+  const [name,setName] = useState("");
+  const handleSubmitIn = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+  const handleSubmitUP =(e)=>{
+    e.preventDefault(name);
+    console.log(name);
+  }
+  const toggleForm=(formname)=>{
+    setCurrentFrom(formname);
+  }
   return (
-    <div className="App">
-      
+    <div className='auth-form-container'>
+      {
+      currentForm === 'login' ? <SignIn onFormSwitch={toggleForm} email={email} setEmail={setEmail} pass={pass} setPass={setPass} handleSubmitIn={handleSubmitIn}/> : <SignUp email={email} name={name} setName={setName} onFormSwitch={toggleForm} setEmail={setEmail} pass={pass} setPass={setPass} handleSubmitUp={handleSubmitUP}/>
+      }
     </div>
   );
 }
 
-export default App;
+export default App;
